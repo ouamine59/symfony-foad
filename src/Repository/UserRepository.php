5c9db9415a7f792fec3d2b3e5ba9cf2e0e54b4a1
mohamed manseur <mohamedmanseur@MacBook-Pro-de-mohamed.local>
2024-10-28 12:08:49 +0100

    test linter by tibo

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -42,7 +42,7 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         ->getResult();
     }
     //    @return User[] Returns an array of User objects
-    //    
+    //
     //    public function findByExampleField($value): array
     //    {
     //        return $this->createQueryBuilder('u')
