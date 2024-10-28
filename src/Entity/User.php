b8f5028d1f88f808ce5a86753c278f7c4e816a1f
mohamed manseur <mohamedmanseur@MacBook-Pro-de-mohamed.local>
2024-10-28 14:48:24 +0100

    chore(user): test commit

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -116,5 +116,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+        // couc
     }
 }
